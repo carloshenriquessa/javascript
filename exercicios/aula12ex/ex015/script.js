57e7c3d9e246e227fcc1f1693895418e3aef4c55
Carlos Henrique <carloshenrique4516@gmail.com>
2025-09-08 22:35:40 -0300

    qual idade

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -9,12 +9,33 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if(fsex[0].checked){
             genero = 'Homem'
+            if(idade >= 0 && idade < 12){
+                img.setAttribute('src', 'imagens/bebe-m.jpg')
+            } else if(idade < 21){
+                img.setAttribute('src', 'imagens/jovem-m.jpg')
+            } else if(idade < 50){
+                img.setAttribute('src', 'imagens/adulto-m.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/idoso-m.jpg')
+            }
         } else if (fsex[1].checked){
             genero = 'Mulher'
+            if(idade >= 0 && idade < 12){
+                img.setAttribute('src', 'imagens/bebe-f.jpg')
+            } else if(idade < 21){
+                img.setAttribute('src', 'imagens/jovem-f.jpg')
+            } else if(idade < 50){
+                img.setAttribute('src', 'imagens/adulto-f.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/idoso-f.jpg')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
